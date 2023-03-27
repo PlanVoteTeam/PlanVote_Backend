bd1597dda352ee92773ca8b78d593337ab23c245
Dylan ANTONIOTTI <dylan.antoniotti@gmail.com>
2023-03-27 12:22:47 +0200

    feat(TimeSlot): create a time slot

--- src/time-slot/time-slot.module.ts
+++ src/time-slot/time-slot.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TimeSlotService } from './time-slot.service';
 import { TimeSlotController } from './time-slot.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { EventSchema } from 'src/schemas/event.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Event', schema: EventSchema }]),
+  ],
   controllers: [TimeSlotController],
-  providers: [TimeSlotService]
+  providers: [TimeSlotService],
 })
 export class TimeSlotModule {}
--- src/time-slot/time-slot.service.ts
+++ src/time-slot/time-slot.service.ts
@@ -1,11 +1,65 @@
 import { Injectable } from '@nestjs/common';
+import { BadRequestException } from '@nestjs/common/exceptions';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { IEvent } from 'src/events/interfaces/event.interface';
+import { IParticipant } from 'src/events/interfaces/participant.interface';
+import { ITimeSlot } from 'src/events/interfaces/time-slot.interface';
 import { CreateTimeSlotDto } from './dto/create-time-slot.dto';
 import { UpdateTimeSlotDto } from './dto/update-time-slot.dto';
+import {
+  ERROR_CODE_INVALID_DATE_TIME_SLOT,
+  ERROR_CODE_MISSING_DATE_TIME_SLOT,
+} from './time-slot.error-code';
+import {
+  ERROR_MESSAGE_INVALID_DATE_TIME_SLOT,
+  ERROR_MESSAGE_MISSING_DATE_TIME_SLOT,
+} from './time-slot.error-message';
 
 @Injectable()
 export class TimeSlotService {
-  create(createTimeSlotDto: CreateTimeSlotDto) {
-    return 'This action adds a new timeSlot';
+  constructor(@InjectModel('Event') private eventModel: Model<IEvent>) {}
+  async create(
+    eventId: string,
+    participantId: string,
+    createTimeSlotDto: CreateTimeSlotDto,
+  ): Promise<ITimeSlot[]> {
+    if (!createTimeSlotDto.startDate || !createTimeSlotDto.endDate) {
+      throw new BadRequestException({
+        success: false,
+        errorCode: ERROR_CODE_MISSING_DATE_TIME_SLOT,
+        errorMessage: ERROR_MESSAGE_MISSING_DATE_TIME_SLOT,
+      });
+    }
+
+    if (createTimeSlotDto.startDate > createTimeSlotDto.endDate) {
+      throw new BadRequestException({
+        success: false,
+        errorCode: ERROR_CODE_INVALID_DATE_TIME_SLOT,
+        errorMessage: ERROR_MESSAGE_INVALID_DATE_TIME_SLOT,
+      });
+    }
+    const timeSlotCreated = await this.eventModel.findOneAndUpdate(
+      { _id: eventId, 'participants._id': participantId },
+      {
+        $push: {
+          'participants.$.timeSlots': createTimeSlotDto,
+        },
+      },
+      {
+        new: true,
+      },
+    );
+
+    console.log(timeSlotCreated.participants[0]._id);
+    console.log(participantId);
+
+    const timeSlots = timeSlotCreated.participants.filter(
+      (participant: IParticipant) =>
+        participant._id.toString() === participantId,
+    )[0].timeSlots;
+
+    return timeSlots;
   }
 
   findAll() {
