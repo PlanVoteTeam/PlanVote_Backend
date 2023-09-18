a7613697a42c03f09901bb9cb3131b67a085fa9d
Dylan ANTONIOTTI <dylan.antoniotti@gmail.com>
2023-09-18 21:08:26 +0200

    fix(cors): add netlify url to origin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://planvote.netlify.app/'],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   });
   app.use(helmet());
