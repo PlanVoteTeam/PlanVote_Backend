2bd0d18032d18fccfe5f724b2c81fe769ac103dd
Dylan ANTONIOTTI <dylan.antoniotti@gmail.com>
2023-09-18 21:25:56 +0200

    fix(cors): add allowed headers

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,14 @@ async function bootstrap() {
   app.enableCors({
     origin: ['http://localhost:5173', 'https://planvote.netlify.app/'],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    allowedHeaders: [
+      'Content-Type',
+      'X-CSRF-TOKEN',
+      'Access-Control-Allow-Methods',
+      'Access-Control-Allow-Origin',
+      'Access-Control-Allow-Credentials',
+      'Access-control-allow-headers',
+    ],
   });
   app.use(helmet());
   await app.listen(3000);
