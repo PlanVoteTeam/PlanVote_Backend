03b35f6f742bd1cd3ff208db85817c36278bc7c3
Dylan ANTONIOTTI <dylan.antoniotti@gmail.com>
2023-09-18 21:31:51 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,32 @@ import { AppModule } from './app.module';
 import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['http://localhost:5173', 'https://planvote.netlify.app/'],
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    allowedHeaders: [
-      'Content-Type',
-      'X-CSRF-TOKEN',
-      'Access-Control-Allow-Methods',
-      'Access-Control-Allow-Origin',
-      'Access-Control-Allow-Credentials',
-      'Access-control-allow-headers',
-    ],
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ['http://localhost:5173', 'https://planvote.netlify.app/'],
+      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+      allowedHeaders: [
+        'Content-Type',
+        'X-CSRF-TOKEN',
+        'Access-Control-Allow-Methods',
+        'Access-Control-Allow-Origin',
+        'Access-Control-Allow-Credentials',
+        'Access-control-allow-headers',
+      ],
+    },
   });
+  // app.enableCors({
+  //   origin: ['http://localhost:5173', 'https://planvote.netlify.app/'],
+  //   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  //   allowedHeaders: [
+  //     'Content-Type',
+  //     'X-CSRF-TOKEN',
+  //     'Access-Control-Allow-Methods',
+  //     'Access-Control-Allow-Origin',
+  //     'Access-Control-Allow-Credentials',
+  //     'Access-control-allow-headers',
+  //   ],
+  // });
   app.use(helmet());
   await app.listen(3000);
 }
